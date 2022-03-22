ffd1ee6cb00a09438856080afafb409c155a0348
Jennifer Melzer <phuong17889@gmail.com>
2022-03-22 11:23:10 +0700

    Update m141015_171156_email_template.php

--- src/migrations/m141015_171156_email_template.php
+++ src/migrations/m141015_171156_email_template.php
@@ -7,18 +7,21 @@ class m141015_171156_email_template extends Migration
 
     public function up()
     {
-        $this->createTable('{{%email_template}}', [
-            'id' => $this->primaryKey(),
-            'shortcut' => $this->string()->notNull(),
-            'from' => $this->string()->notNull(),
-            'subject' => $this->string(),
-            'text' => $this->text()->notNull(),
-            'language' => $this->string(),
-        ], 'Engine=InnoDB');
-        $this->createIndex('shortcut', '{{%email_template}}', [
-            'shortcut',
-            'language',
-        ], true);
+	    try {
+		    $this->createTable('{{%email_template}}', [
+			    'id' => $this->primaryKey(),
+			    'shortcut' => $this->string()->notNull(),
+			    'from' => $this->string()->notNull(),
+			    'subject' => $this->string(),
+			    'text' => $this->text()->notNull(),
+			    'language' => $this->string(),
+		    ], 'Engine=InnoDB');
+		    $this->createIndex('shortcut', '{{%email_template}}', [
+			    'shortcut',
+			    'language',
+		    ], true);
+		} catch (Exception|Error $e) {
+	    }
     }
 
     public function down()
