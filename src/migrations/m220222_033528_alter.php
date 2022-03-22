dac544c7e6dd92766ebaa227d1aa1b71aa26dd46
Jennifer Melzer <phuong17889@gmail.com>
2022-03-22 11:22:19 +0700

    Update m220222_033528_alter.php

--- src/migrations/m220222_033528_alter.php
+++ src/migrations/m220222_033528_alter.php
@@ -13,12 +13,15 @@ class m220222_033528_alter extends Migration
      */
     public function safeUp()
     {
-        $this->addColumn('{{%email_message}}', 'email_template_id', \yii\db\Schema::TYPE_INTEGER . ' DEFAULT NULL');
+	    try {
+		    $this->addColumn('{{%email_message}}', 'email_template_id', \yii\db\Schema::TYPE_INTEGER . ' DEFAULT NULL');
+	    } catch (Exception|Error $e) {
+	    }
         $this->alterColumn('{{%email_message}}', 'status', \yii\db\Schema::TYPE_STRING . '(255) NOT NULL DEFAULT "0"');
-        $this->update('{{%email_message}}', ['status' => \phuong17889\email\models\EmailMessage::STATUS_NEW], ['status' => 0]);
-        $this->update('{{%email_message}}', ['status' => \phuong17889\email\models\EmailMessage::STATUS_IN_PROGRESS], ['status' => 1]);
-        $this->update('{{%email_message}}', ['status' => \phuong17889\email\models\EmailMessage::STATUS_SENT], ['status' => 2]);
-        $this->update('{{%email_message}}', ['status' => \phuong17889\email\models\EmailMessage::STATUS_ERROR], ['status' => 3]);
+        $this->update('{{%email_message}}', ['status' => \phuong17889\email\models\EmailMessage::STATUS_NEW], ['status' => '0']);
+        $this->update('{{%email_message}}', ['status' => \phuong17889\email\models\EmailMessage::STATUS_IN_PROGRESS], ['status' => '1']);
+        $this->update('{{%email_message}}', ['status' => \phuong17889\email\models\EmailMessage::STATUS_SENT], ['status' => '2']);
+        $this->update('{{%email_message}}', ['status' => \phuong17889\email\models\EmailMessage::STATUS_ERROR], ['status' => '3']);
         $this->alterColumn('{{%email_message}}', 'status', "ENUM('error','new','in_progress','sent') NOT NULL DEFAULT 'new'");
     }
 
