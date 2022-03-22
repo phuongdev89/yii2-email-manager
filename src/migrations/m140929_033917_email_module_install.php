955e42203b9984dd444cc3ed8bb0c49afaad7091
Jennifer Melzer <phuong17889@gmail.com>
2022-03-22 11:23:33 +0700

    Update m140929_033917_email_module_install.php

--- src/migrations/m140929_033917_email_module_install.php
+++ src/migrations/m140929_033917_email_module_install.php
@@ -7,19 +7,22 @@ class m140929_033917_email_module_install extends Migration
 
     public function up()
     {
-        $this->createTable('{{%email_message}}', [
-            'id' => $this->primaryKey(),
-            'status' => $this->integer()->notNull()->defaultValue(0),
-            'priority' => $this->integer()->notNull()->defaultValue(0),
-            'from' => $this->string(),
-            'to' => $this->string(),
-            'subject' => $this->string(),
-            'text' => $this->text(),
-            'created_at' => $this->integer(),
-            'sent_at' => $this->integer(),
-            'bcc' => $this->text(),
-            'files' => $this->text(),
-        ], 'Engine=InnoDB');
+	    try {
+		    $this->createTable('{{%email_message}}', [
+			    'id' => $this->primaryKey(),
+			    'status' => $this->integer()->notNull()->defaultValue(0),
+			    'priority' => $this->integer()->notNull()->defaultValue(0),
+			    'from' => $this->string(),
+			    'to' => $this->string(),
+			    'subject' => $this->string(),
+			    'text' => $this->text(),
+			    'created_at' => $this->integer(),
+			    'sent_at' => $this->integer(),
+			    'bcc' => $this->text(),
+			    'files' => $this->text(),
+		    ], 'Engine=InnoDB');
+	    } catch (Exception|Error $e) {
+	    }
     }
 
     public function down()
